55061ff5bfc3908e18d3a5f628daf34a919805d6
faustaleonardo <faustaleonardo11@gmail.com>
2021-04-26 20:41:00 +0700

    finished up setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map',
   entry: './src/index.ts',
   module: {
     rules: [
@@ -11,8 +13,16 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    publicPath: '/',
+    contentBase: './public',
+    hot: true,
   },
 };
